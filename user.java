c68efd1d35fd46baad9656f33d0cf4b119493506
adamhusted <hustedaa@g.cofc.edu>
2023-09-21 18:22:51 -0400

    Updated User.java

---
+++ user.java
@@ -0,0 +1,13 @@
+public class User {
+    // searchEvent(eventId: string)
+    String username;
+    String password;
+
+    public String searchEvent(String eventId) {
+        // call ticket class to get ticket info
+        String ticketInfo = "";
+
+        return ticketInfo;
+    }
+
+}
