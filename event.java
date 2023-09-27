1eca886e850f1bf56b1264ddb44ef528b4738b85
adamhusted <hustedaa@g.cofc.edu>
2023-09-26 23:57:41 -0400

    Create event.jave

---
+++ event.java
@@ -0,0 +1,10 @@
+public class event {
+    String[] foundEvents;
+    
+    public int[] searchTickets(String[] events) {
+        // calls the tickets class to get the ticketIDs of the events returned from original event search
+
+        int[] eventTickets = new int[events.length];
+        return eventTickets;
+    }
+}
