aaa21be2c1843abeb5577d63caa208ddfd48bc96
adamhusted <hustedaa@g.cofc.edu>
2023-09-26 23:34:48 -0400

    Finished User class and created ticket.java

--- user.java
+++ user.java
@@ -1,14 +1,41 @@
-public class User {
+public class user {
     // searchEvent(eventId: string)
     String username;
     String password;
+    String firstName;
+    String lastName;
+    String email;
+    String[] cart;
 
-    public String searchEvent(String eventId) {
-        // call ticket class to get ticket info
-        String ticketInfo = "";
-
-        return ticketInfo;
-        // test
+    public boolean loginUser(String username, String password) {
+        // takes user and password and finds if the the pairing is valid
+        boolean confirm = false;
+        return confirm;
     }
+
+    public boolean registerUser(String username, String password, String firstName, String lastName, String email) {
+        // creates a new user class and stores into the db using the parameters
+        boolean success = true;
+        return success;
+    }
+
+    public String[] searchEvent(String eventId) {
+        // call event class to get ticket info
+        String[] events = new String[]{"Event1", "Evnent2"};
+        return events;
+    }
+
+    public void addToCart(int ticketID) {
+        // takes selected tickets and adds to cart
+    }
+
+    public void purchaseTickets(String[] cart) {
+        // takes cart and purchases the tickets by calling the ticket class
+    }
+
+    public void displayTicket(String[] purchasedTickets) {
+        // displays tickets returned after purchasing
+    }
+
 
 }
